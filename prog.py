66d216e05385d6a7ac89c68d4737f9239b546abe
Safoine REJEB <109766518+Space-Cruiser98@users.noreply.github.com>
2022-08-15 11:26:43 +0000

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,37 +1,31 @@
 from random import randint as r
-x = int(input("How many characters would you like your password to be ? Choose between 8 and 24 "))
+x = int(input("How many characters would you like your password to be ? Choose between 8 and 24 : "))
 while not (8<=x<=24):
   x = int(input("Please choose a length between 8 and 24"))
-#Declaring the result variable as a sequence of zeros
-pw = ""
-for i in range(x):
-  pw+="0"
-  
-s = x // 4 
+pw =""
 
-#Assigning character function
-def assign(a,b):
-  for i in range (s):
-  c = chr(r(a,b))
-  k = pw[r(0,x)]
-  while k != "0" :
-    k = c
-  
-#Uppercase alphabetic characters
-assign(65,90)
+s= x//4 
 
-#Lowercase alphabetic characters
-assign(97,122)
+def func1(a,b):
+  f=""
+  for i in range(s):
+    f = f + chr(r(a,b))
+  return f 
 
-#Numerical characters
-assign(48,57)
 
-#Dealing with special characters
-#Declaring a list containing the ascii code intervals of symbols then randomly choosing which interval will be given to the assign function 
-l = [(32,47),(58,64),(91,96),(123,126)]
-assign(r(0,3))
 
-#Displaying result
+def func2():
+  l1 = [32,58,91,123]
+  l2 = [47,64,96,126]
+  x= r(0,3)
+  y = x 
+  return func1(l1[x],l2[y])
+
+pw = func1(65, 90) + func1(97, 122) + func1(48, 57) + func2()
+
+if len(pw) < x :
+  k= x - len(pw)
+  for i in range (k):
+    pw = pw + chr(r(65, 90))
 
 print ("The generated password for you is : ", pw)
-    
